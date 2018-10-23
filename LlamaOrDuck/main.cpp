4477e36ed326d8a3746a9a2a3750ec8db863637f
ayushrath63 <ayushrath63@github.com>
2018-10-23 16:54:35 -0700

    Update LED blick frequency

--- LlamaOrDuck/main.cpp
+++ LlamaOrDuck/main.cpp
@@ -17,8 +17,8 @@ int main() {
     while(1) {
         transmitter.write(5, data, 1);
         myled = 1; // LED is ON
-        wait(0.2); // 200 ms
+        wait(0.1); // 200 ms
         myled = 0; // LED is OFF
-        wait(1.0); // 1 sec
+        wait(0.1); // 1 sec
     }
 }
