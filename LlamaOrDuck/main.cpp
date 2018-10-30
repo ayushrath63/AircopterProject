dbc3503c52f354c57b3addbede13fd5daf0e79cf
ayushrath63 <ayush.rath@gmail.com>
2018-10-30 14:35:06 -0700

    Fix TX/RX issues

--- LlamaOrDuck/main.cpp
+++ LlamaOrDuck/main.cpp
@@ -1,9 +1,7 @@
 
 #include "mbed.h"
-#include "nRF24L01P.h"
 #include "millis.h"
-#include <cstdio>
-#include <cstdlib>
+#include "nRF24L01P.h"
 
 #define TRANSFER_SIZE 1
 #define RED 0x05
@@ -11,16 +9,10 @@
 #ifdef RECEIVE
 
 nRF24L01P my_nrf24l01p(PB_5, PB_4, PB_3, PA_4, PF_0);    // mosi, miso, sck, csn, ce, irq
-Serial printer(SERIAL_TX,SERIAL_RX);
-/*
 DigitalOut ledR(A5);
 DigitalOut ledG(A6);
-*/
+Serial printer(SERIAL_TX,SERIAL_RX);
 
-DigitalOut ledR(D9);
-DigitalOut ledG(A2);
-DigitalIn btnR(A6);
-DigitalIn btnG(A5);
 
 int main() {
 
@@ -30,62 +22,54 @@ int main() {
     my_nrf24l01p.setRfOutputPower(); //Done
     my_nrf24l01p.setAirDataRate();   //Done
     my_nrf24l01p.setCrcWidth();      //Done
-    my_nrf24l01p.setRxAddress(0xc2c2c2c2c2);
-    my_nrf24l01p.setTxAddress(0xe7e7e7e7e7);
+    my_nrf24l01p.setTxAddress(0xc2c2c2c2c2);
+    my_nrf24l01p.setRxAddress(0xe7e7e7e7e7);
     my_nrf24l01p.setTransferSize( TRANSFER_SIZE );
 
     my_nrf24l01p.setReceiveMode();
     my_nrf24l01p.enable();
     
     char buffer = 0;
-    millis_begin();
-    while (1)
-    {
-        if(buffer != 0)
-            printer.printf("%d\r\n", buffer);
-        if(my_nrf24l01p.readable())
-        {
+    
+    while (1) {
+        ledR = 0;
+        ledG = 0;
+        wait(1);
+        if(my_nrf24l01p.readable()){
             my_nrf24l01p.read(0, &buffer, 1);
-            if(buffer == RED)
-            {
-                ledR = 0;
-                ledG = 1;
-            }
-            else if(buffer == GREEN)
-            {
+            if(buffer == 0x05){
                 ledR = 1;
-                ledG = 0;
+                printer.printf("%d", buffer);
+                wait(1);
+            } else if(buffer == 0x05){
+                ledG = 1;
+                printer.printf("%d", buffer);
+                wait(1);
             }
         }
     }
 }
 
 #endif
-
+//
 #ifdef TRANSMIT
 
 #include "mbed.h"
 #include "nRF24L01P.h"
-
+//
 nRF24L01P my_nrf24l01p(PB_5, PB_4, PB_3, PA_4, PF_0);    // mosi, miso, sck, csn, ce, irq
-/*
-DigitalOut ledR(D9);
-DigitalOut ledG(A2);
-DigitalIn btnR(A6);
-DigitalIn btnG(A5);
-*/
-DigitalOut ledR(A5);
-DigitalOut ledG(A6);
+
 
 int main() {
+
     //Set up for Send
     my_nrf24l01p.powerUp();
     my_nrf24l01p.setRfFrequency(NRF24L01P_MIN_RF_FREQUENCY + 16);
     my_nrf24l01p.setRfOutputPower(); //Done
     my_nrf24l01p.setAirDataRate();   //Done
     my_nrf24l01p.setCrcWidth();      //Done
-    my_nrf24l01p.setRxAddress(0xe7e7e7e7e7);
-    my_nrf24l01p.setTxAddress(0xc2c2c2c2c2);
+    my_nrf24l01p.setTxAddress(0xe7e7e7e7e7);
+    my_nrf24l01p.setRxAddress(0xc2c2c2c2c2);
     my_nrf24l01p.setTransferSize( TRANSFER_SIZE );
 
     my_nrf24l01p.setTransmitMode();
@@ -93,30 +77,10 @@ int main() {
 
     char red = RED;
     char green = GREEN;
-    srand(0xDEAD);
-    millis_begin();
-    int presstime = 2000;
-    while (1)
-    {
-            my_nrf24l01p.write(0, &red, 1);
-        /*
-        if(rand() & 1)
-        {
-            ledR = 1;
-            ledG = 0;
-        }
-        else
-        {
-            ledG = 1;
-            ledR = 0;
-        }
-        while(!btnR && !btnG);
-
-        while(!btnR && !btnG);
-        if(btnR == 1 && ledR == 1) my_nrf24l01p.write(0, &red, 1);
-        else if(btnG == 1 && ledG == 1) my_nrf24l01p.write(0, &green, 1);
-        wait(0.1);
-        */
+    
+    while (1) {
+        if(rand() % 2) my_nrf24l01p.write(0, &red, 1); 
+        else my_nrf24l01p.write(0, &green, 1); 
     }
 }
 #endif
